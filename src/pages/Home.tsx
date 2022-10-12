5dbe6fd695e47b54886ed58fa96f886f1b82a1db
yashweblife <yashweblife20@gmail.com>
2022-10-12 12:13:24 -0500

    Experimental reorder system

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import { IonContent, IonHeader, IonItem, IonLabel, IonPage, IonReorder, IonReorderGroup, IonTitle, IonToolbar, ItemReorderEventDetail } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonContent, IonHeader, IonItem, IonLabel, IonPage, IonReorder, IonReorderGroup, IonTitle, IonToolbar, ItemReorderEventDetail } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const handleReorder = (event:CustomEvent<ItemReorderEventDetail>)=>{
+    event.detail.complete()
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -10,17 +13,49 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonReorderGroup disabled={false}>
-          <IonReorder>
-            <IonItem>
-              <IonLabel>T1</IonLabel>
-            </IonItem>
-          </IonReorder>
-          <IonReorder>
-            <IonItem>
-              <IonLabel>T2</IonLabel>
-            </IonItem>
-          </IonReorder>
+        <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
+          <IonCard>
+            <IonCardHeader>
+              <IonReorder>
+                <IonTitle>Test 1</IonTitle>
+              </IonReorder>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
+                <IonItem>
+                  <IonReorder>
+                    <IonLabel>Hello 1</IonLabel>
+                  </IonReorder>
+                </IonItem>
+                <IonItem>
+                  <IonReorder>
+                    <IonLabel>Hello 2</IonLabel>
+                  </IonReorder>
+                </IonItem>
+              </IonReorderGroup>
+            </IonCardContent>
+          </IonCard>
+          <IonCard>
+            <IonCardHeader>
+              <IonReorder>
+                <IonTitle>Test 2</IonTitle>
+              </IonReorder>
+            </IonCardHeader>
+            <IonCardContent>
+              <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
+                <IonItem>
+                  <IonReorder>
+                    <IonLabel>World 1</IonLabel>
+                  </IonReorder>
+                </IonItem>
+                <IonItem>
+                  <IonReorder>
+                    <IonLabel>World 2</IonLabel>
+                  </IonReorder>
+                </IonItem>
+              </IonReorderGroup>
+            </IonCardContent>
+          </IonCard>
         </IonReorderGroup>
       </IonContent>
     </IonPage>
